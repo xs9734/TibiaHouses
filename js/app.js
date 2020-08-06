6b282a2660f1e50abf0629e9003c91fd9acd2dc3
David <42820350+xs9734@users.noreply.github.com>
2020-08-06 02:11:28 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -90,7 +90,7 @@ function abDendriel(){
           var houseRent = allHomes[i].rent;
           var houseStatus = allHomes[i].status;
           var badge = document.createElement('div');
-            badge.className = 'badge';
+            badge.className = 'badge large-2 medium-4 small-12';
             badge.innerHTML =
               '<h2 class="names">' + houseName + '</h2>' +
               '<h3 class="rents">' + houseRent + '</h3>' +
